6bc95fb1d5ecd88a3d4e30ce91bcc8e11431b7a6
shantanu-earlier-git-hub <webtec76@gmail.com>
2026-04-30 20:38:24 -0400

    init

---
+++ my-spring-boot-projs/jpa-basic-1/src/main/java/jpa/app/JpaBasic1Application.java
@@ -0,0 +1,14 @@
+package jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JpaBasic1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBasic1Application.class, args);
+    }
+
+}
